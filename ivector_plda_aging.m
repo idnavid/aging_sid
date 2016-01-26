858b0ed5a666e23c90e01f09acdcea27bc374bd9
Navid Shokouhi <navid.shokouhi@utdallas.edu>
2016-01-26 16:00:54 -0600

    loads pretrained params.

--- ivector_plda_aging.m
+++ ivector_plda_aging.m
@@ -1,6 +1,7 @@
 clc
 clear
 addpath(genpath('/home/nxs113020/Downloads/MSR_Identity_dir/MSR_Identity_Toolkit_v1.0/code/'));
+
 %% Step0: Opening MATLAB pool
 nworkers = 12;
 nworkers = min(nworkers, feature('NumCores'));
@@ -8,40 +9,43 @@ isopen = matlabpool('size')>0;
 if ~isopen, matlabpool(nworkers); end
 
 %% Step1: Training the UBM
-dataList = 'ubm.lst';
-nmix        = 256;
-final_niter = 10;
-ds_factor   = 1;
-ubm = gmm_em(dataList, nmix, final_niter, ds_factor, nworkers);
+ubm_location = 'Aging/mf_UBM';
+tmp = load(ubm_location);
+ubm = tmp.ubm;
+nmix = length(ubm.w);
+clear tmp;
 
 %% Step2: Learning the total variability subspace from background data
-tv_dim = 400; 
-niter  = 5;
-dataList = 'ubm.lst';
-fid = fopen(dataList, 'rt');
-C = textscan(fid, '%s');
-fclose(fid);
-feaFiles = C{1};
-stats = cell(length(feaFiles), 1);
-parfor file = 1 : length(feaFiles),
-    [N, F] = compute_bw_stats(feaFiles{file}, ubm);
-    stats{file} = [N; F];
-end
-T = train_tv_space(stats, ubm, tv_dim, niter, nworkers);
+tv_location = 'Aging/mf_T';
+tmp = load(tv_location);
+T = tmp.T;
+tv_dim = size(T,1); 
+clear tmp
 
 %% Step3: Training the Gaussian PLDA model with development i-vectors
 lda_dim = 200;
 nphi    = 200;
 niter   = 10;
-dataList = 'ubm_with_inds.lst';
+dataList = 'lists/dev_list';
 fid = fopen(dataList, 'rt');
 C = textscan(fid, '%s %s');
 fclose(fid);
 feaFiles = C{1};
-dev_ivs = zeros(tv_dim, length(feaFiles));
-parfor file = 1 : length(feaFiles),
-    dev_ivs(:, file) = extract_ivector(stats{file}, ubm, T);
-end
+#dev_ivs = zeros(tv_dim, length(feaFiles));
+
+#stats = cell(length(feaFiles), 1);
+#parfor file = 1 : length(feaFiles),
+#    [N, F] = compute_bw_stats(feaFiles{file}, ubm);
+#    stats{file} = [N; F];
+#end 
+
+#parfor file = 1 : length(feaFiles),
+#    dev_ivs(:, file) = extract_ivector(stats{file}, ubm, T);
+#end
+
+#save('dev_ivectors','dev_ivs')
+#save('stats','stats')
+#pause
 % reduce the dimensionality with LDA
 spk_labs = C{2};
 V = lda(dev_ivs, spk_labs);
