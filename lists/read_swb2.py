eb78d83fe52cf15910466baf04b8ecc17f2ae2a9
nav <navid.shokouhi@utdallas.edu>
2016-03-01 10:28:38 -0600

    added ubm (nist05,06) and plda (swb2-male) lists. Two age groups: under 20, 22 and older.

---
+++ lists/read_swb2.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python 
+
+
+fcallinfo = open('swb2_callinfo.tbl')
+call_dict = {}
+for i in fcallinfo:
+    line_list = i.strip().split(',')
+    fileid = line_list[0]
+    spkrid = line_list[2]
+    spkrch = line_list[3]
+    if not(spkrid in call_dict):
+        call_dict[spkrid] = [fileid+spkrch]
+    else:
+        call_dict[spkrid].append(fileid+spkrch)
+
+fcallinfo.close()
+
+fsphlist = open('all_swb2.lst')
+file_dict = {}
+for i in fsphlist:
+    line = i.strip()
+    line_list = line.split('/')
+    fileid = line_list[-1][:-4]
+    file_dict[fileid] = line
+fsphlist.close()
+
+fspkrinfo = open('swb2_male_spkrs.tbl')
+spkr_dict = {}
+for i in fspkrinfo:
+    line_list = i.strip().split(',')
+    spkrid = line_list[0]
+    spkrgender = line_list[1]
+    spkrage = line_list[2]
+    try:
+        if int(spkrage) < 20:
+            for j in call_dict[spkrid]:
+                print file_dict[j]+' '+spkrid
+    except:
+        pass
+
+
+
+
+
